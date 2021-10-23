e68c737214d8d5f50d6bd407caf8dd241ac5e749
Ben <71402472+btims512@users.noreply.github.com>
2021-10-22 20:15:55 -0600

    added icons & likes

--- src/Components/UserPosts/Post.js
+++ src/Components/UserPosts/Post.js
@@ -4,6 +4,10 @@ import stories from "../../img/stories.png";
 import firebase from "firebase";
 import { db } from "../firebase/firebase";
 import Avatar from "@material-ui/core/Avatar";
+import NavBarIcons from "../NavBar/NavBarIcons";
+import { FaRegComment, FaRegHeart } from "react-icons/fa";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { BsThreeDots } from "react-icons/bs";
 
 function Post(props) {
   const { postId, user, username, caption, imageUrl } = props;
@@ -39,17 +43,20 @@ function Post(props) {
   return (
     <div src={stories} className="post">
       <div className="post_header">
-        <Avatar
-          className="post_avatar"
-          alt={username}
-          src={username}
-        />
-        <h3>{username}</h3>
+        <Avatar className="post_avatar" alt={username} src={username} />
+        <h3>{username}</h3>{" "}
+        <BsThreeDots style={{ cursor: "pointer", paddingLeft: "70%" }} />
       </div>
-
+      {/* <MoreHorizIcon style={{ display: "flex", justifyContent: "center" }} /> */}
       {/* image */}
       <img className="post_image" src={imageUrl} alt="post" />
       {/* username + caption */}
+      <NavBarIcons>
+        <FaRegHeart size="25" style={{ padding: "14px" }} />
+        <FaRegComment size="25" style={{ padding: "14px" }} />
+        <IoPaperPlaneOutline size="28" style={{ padding: "12px" }} />
+      </NavBarIcons>{" "}
+      <h4 style={{ paddingLeft: "20px", fontWeight: "600" }}>423 likes</h4>
       <h4 className="post_text">
         <strong>{username}</strong>
         {caption}
@@ -62,7 +69,6 @@ function Post(props) {
             </p>
           ))}
       </div>
-
       {user && (
         <form className="post_commentBox">
           <input
